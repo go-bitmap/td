4f9321221865cb2cf4195923e625f96da191fe18
it00021hot <liu15077731547@gmail.com>
2025-03-02 17:15:56 +0800

    feat: qrLogin

--- telegram/custom_client.go
+++ telegram/custom_client.go
@@ -1,9 +1,12 @@
 package telegram
 
 import (
+	"fmt"
+	"github.com/go-faster/errors"
 	"github.com/gotd/td/bin"
 	"github.com/gotd/td/mtproto"
 	"github.com/gotd/td/tg"
+	"go.uber.org/zap"
 )
 
 type CustomClient struct {
@@ -28,5 +31,30 @@ func (c *customHandler) OnSession(cfg tg.Config, s mtproto.Session) error {
 }
 
 func (c *customHandler) OnMessage(b *bin.Buffer) error {
-	return c.client.handleUpdates(b)
+	updates, err := tg.DecodeUpdates(b)
+	if err != nil {
+		return errors.Wrap(err, "decode updates")
+	}
+	switch u := updates.(type) {
+	case *tg.Updates:
+		c.client.updateInterceptor(u.Updates...)
+		return c.client.updateHandler.Handle(c.client.ctx, u)
+	case *tg.UpdatesCombined:
+		c.client.updateInterceptor(u.Updates...)
+		return c.client.updateHandler.Handle(c.client.ctx, u)
+	case *tg.UpdateShort:
+		c.client.updateInterceptor(u.Update)
+		return c.client.updateHandler.Handle(c.client.ctx, u)
+	case *tg.UpdateShortMessage:
+		return c.client.updateHandler.Handle(c.client.ctx, u)
+	case *tg.UpdateShortChatMessage:
+		return c.client.updateHandler.Handle(c.client.ctx, u)
+	case *tg.UpdateShortSentMessage:
+		return c.client.updateHandler.Handle(c.client.ctx, u)
+	case *tg.UpdatesTooLong:
+		return c.client.updateHandler.Handle(c.client.ctx, u)
+	default:
+		c.client.log.Warn("Ignoring update", zap.String("update_type", fmt.Sprintf("%T", u)))
+		return c.client.updateHandler.Handle(c.client.ctx, u)
+	}
 }
